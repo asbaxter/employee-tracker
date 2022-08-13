6786de3b9ed0eadee34bc048d4579520e9acb729
Andrew Baxter <asbaxter1@gmail.com>
2022-08-12 23:14:01 -0400

    set up basic database with tables for department, role and employee

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,24 @@
+INSERT INTO department
+  (name)
+VALUES
+  ('Sales'),
+  ('Engineering'),
+  ('Legal'),
+  ('Finance');
+
+INSERT INTO role
+    (title, salary)
+VALUES
+    ('Sales Rep', 75000.00),
+    ('Sales Lead', 125000.00),
+    ('Lead Engineer', 250000.00),
+    ('Software Engineer', 150000.00),
+    ('Lawyer', 150000.00),
+    ('Legal Team Lead', 200000.00),
+    ('Accountant', 125000.00),
+    ('Account Manager', 200000.00);
+ 
+
 INSERT INTO employee
   (first_name, last_name, role_id, manager_id)
 VALUES
