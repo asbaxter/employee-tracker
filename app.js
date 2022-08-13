4dece315dcb015808cb6ce124527dff51b4ef3bd
Andrew Baxter <asbaxter1@gmail.com>
2022-08-13 13:06:01 -0400

    added other menu options and seperated into functions for cleaner code

--- app.js
+++ app.js
@@ -8,39 +8,71 @@ function promptUser(){
       {
         type: "list",
         message: "What would you like to do:",
-        choices: ["View all Employees", "View all Roles", "View all Departments"],
+        choices: [
+          "View all Employees",
+          "Add Employee",
+          "Update Employee Role",
+          "View all Roles",
+          "Add Role",
+          "View all Departments",
+          "Add Department"
+        ],
         name: "menu"
       }
     ])
     .then(function ({ menu }) {
       if (menu == "View all Employees"){
-        const sql = `SELECT * FROM employee`;
-        db.query(sql, (err, res) => {
-          if (err) throw err;
-          console.table(res);
-          promptUser();
-      });
+        allEmployees();
+      }
+      else if(menu == "Add Employee"){
+        console.log("add employee coming soon")
+      }
+      else if(menu == "Update Employee Role"){
+        console.log("Update Employee role comming soon")
       }
       else if(menu == "View all Roles"){
-        const sql = `SELECT * FROM role`;
-        db.query(sql, (err, res) => {
-          if (err) throw err;
-          console.table(res);
-          promptUser();
-      });
+        allRoles();
+      }
+      else if(menu == "Add Role"){
+        console.log("add rolle coming soon");
       }
       else if (menu == "View all Departments"){
-        const sql = `SELECT * FROM department`;
-        db.query(sql, (err, res) => {
-          if (err) throw err;
-          console.table(res);
-          promptUser();
-        });
+        allDepartments();
+      }
+      else if(menu == "Add Department"){
+        console.log("add deparment coming soon")
       }
       else {
         console.log("How did you even select an invalid option?")
       }
     })
+}
+
+function allEmployees(){
+  const sql = `SELECT * FROM employee`;
+        db.query(sql, (err, res) => {
+          if (err) throw err;
+          console.table(res);
+          promptUser();
+      });
+}
+
+function allRoles(){
+  const sql = `SELECT * FROM role`;
+        db.query(sql, (err, res) => {
+          if (err) throw err;
+          console.table(res);
+          promptUser();
+        });
+}
+
+function allDepartments(){
+  const sql = `SELECT * FROM department`;
+        db.query(sql, (err, res) => {
+          if (err) throw err;
+          console.table(res);
+          promptUser();
+        });
 }
 
 promptUser();
